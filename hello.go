23a3fd012ede140968b633411532520a9932d39e
Ted Neward <ted@tedneward.com>
2016-05-07 01:49:51 -0700

    After some additional code bits

--- hello.go
+++ hello.go
@@ -1,7 +1,32 @@
-package main
+package gohello
 
 import "fmt"
 
-func main() {
-	fmt.Printf("Hello, CoDe\n")
+var original = "Hello, CoDe world"
+var Message = original
+
+func PigLatinizer(msg string) (success bool, returnVal string) {
+	success = true
+	returnVal = msg + "way"
+	return
+}
+
+func SayHello() {
+	fmt.Printf("%v\n", Message)
+}
+
+func Reset() {
+	Message = original
+}
+
+func DemoDefer() {
+	fmt.Println("Entering DemoDefer")
+
+	defer fmt.Println("In defer the first time")
+
+	defer func() {
+		fmt.Println("In defer the second time")
+	}()
+
+	fmt.Println("Exiting DemoDefer")
 }
